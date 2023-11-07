852a52011df83405e5a3add15b98bd6e52d53322
ioannisleo <trickle-takes06@icloud.com>
2023-11-07 23:53:36 +0200

    Fix tick counting for processes

--- kernel/trap.c
+++ kernel/trap.c
@@ -53,6 +53,8 @@ trap(struct trapframe *tf)
     if(cpu->id == 0){
       acquire(&tickslock);
       ticks++;
+      if (proc != 0)
+          proc->ticks++;
       wakeup(&ticks);
       release(&tickslock);
     }
